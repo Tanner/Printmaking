2eb94a9439f8689ffc6a8c66205215834c163d69
Tanner Smith <tanner@tssoftware.net>
2012-11-02 14:54:13 -0400

    Write tests for getAdjancentTiles.

--- tests/spec/tile.js
+++ tests/spec/tile.js
@@ -17,4 +17,51 @@ describe("A tile", function() {
 
 		expect(tile.owner).toBe(null);
 	});
+
+	describe("should be able to get its adjacent tiles", function() {
+		var board;
+
+		it("when there are no adjacent tiles", function() {
+			board = new Board(1, 1);
+
+			var neighbors = board.tiles[0][0].getAdjacentTiles();
+
+			expect(neighbors).not.toBe(null);
+			expect(neighbors.length).toBe(0);
+		});
+
+		it("when the tile is in the corner", function() {
+			board = new Board(2, 2);
+
+			var neighbors = board.tiles[0][0].getAdjacentTiles();
+
+			expect(neighbors).not.toBe(null);
+			expect(neighbors.length).toEqual(2);
+
+			expect(neighbors).toContain(board.getTileAtPosition(0, 1));
+			expect(neighbors).toContain(board.getTileAtPosition(1, 0));
+
+			board.tiles[1][1].getAdjacentTiles();
+
+			expect(neighbors).not.toBe(null);
+			expect(neighbors.length).toEqual(2);
+
+			expect(neighbors).toContain(board.getTileAtPosition(0, 1));
+			expect(neighbors).toContain(board.getTileAtPosition(1, 0));
+		});
+
+		it("when the tile is in the center", function() {
+			board = new Board(3, 3);
+
+			var neighbors = board.tiles[1][1].getAdjacentTiles();
+
+			expect(neighbors).not.toBe(null);
+			expect(neighbors.length).toEqual(4);
+
+			expect(neighbors).toContain(board.getTileAtPosition(0, 1));
+			expect(neighbors).toContain(board.getTileAtPosition(2, 1));
+			expect(neighbors).toContain(board.getTileAtPosition(1, 0));
+			expect(neighbors).toContain(board.getTileAtPosition(1, 2));
+		});
+	});
 });
