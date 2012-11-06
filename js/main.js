d9caea6ca6d8af5fd393f9e2df5e8c28c7324bb5
Tanner Smith <tanner@tssoftware.net>
2012-11-06 12:51:36 -0500

    Create board and draw it on the page.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
-var board = new Board(5, 5);
+var board, players, game;
 
-var players = [];
-players.push(new Player("Tanner", "#F00"));
-players.push(new Player("Buzz", "#0F0"));
+$(document).ready(function() {
+	init();
+});
 
-var game = new Letterpress(board, players);
+function init() {
+	board = new Board(5, 5);
+
+	players = [];
+	players.push(new Player("Tanner", "#F00"));
+	players.push(new Player("Buzz", "#0F0"));
+
+	game = new Letterpress(board, players);
+
+	$('#board').append(createBoard(board));
+}
+
+function createBoard(board) {
+	var boardElement = $('<ul class="board">');
+
+	for (var row = 0; row < board.rows; row++) {
+		var rowElement = $('<ul class="row">');
+
+		for (var column = 0; column < board.columns; column++) {
+			rowElement.append(createTile(board.getTileAtPosition(row, column)));
+		}
+
+		rowElement.append('</ul>');
+
+		boardElement.append(rowElement);
+	}
+
+	return boardElement;
+}
+
+function createTile(tile) {
+	return $('<li class="tile" data-row='+tile.row+' data-column='+tile.column+'>'+tile.letter+'</li>');
+}
