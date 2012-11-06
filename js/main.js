2cba15aac97eb91775619abd0b42d78a561d791c
Tanner Smith <tanner@tssoftware.net>
2012-11-05 23:40:29 -0500

    Fix var/method misspellings

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var board = new Board(5, 5);
 
 var players = [];
-player.append(new Player("Tanner", "#F00"));
-player.append(new Player("Buzz", "#0F0"));
+players.push(new Player("Tanner", "#F00"));
+players.push(new Player("Buzz", "#0F0"));
 
 var game = new Letterpress(board, players);
